c8774c75066829fd53ba2fa92ef81ded5da09a81
kauraman25 <kauraman200225@gmail.com>
2025-08-24 11:57:17 +0530

    initial commit

--- client/src/pages/Messages.jsx
+++ client/src/pages/Messages.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Messages = () => {
   return (
-    <div>Messages</div>
-  )
-}
+    <div className="min-h-screen relative bg-slate-50">
+      <div className="max-w-6xl mx-auto p-6">
+        {/* title */}
+        <div className="mb-8">
+          <h1 className="text-3xl font-bold text-slate-900 mb-2">Messages</h1>
+          <p className="text-slate-600">Talk to your friends and family</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Messages
+export default Messages;
